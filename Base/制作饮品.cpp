8613d9ff2dd673107845c88193cab1c5dc83b969
xiaTian <939648675@qq.com>
2025-03-02 23:13:12 +0800

    Learn 2025 - 03 - 02

---
+++ Base/制作饮品.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+class AbstractDrinking {
+    public:
+    //煮
+    virtual void Boil() = 0;
+
+    //冲泡
+    virtual void Brew() = 0;
+
+    //倒入
+    virtual void PourInCup() = 0;
+
+    //加入辅料
+    virtual void PourSomething() = 0;
+
+    //制作
+    void makeDrink() {
+        Boil();
+        Brew();
+        PourInCup();
+        PourSomething();
+        cout << "Drinking the drink" << endl;
+    }
+};
+
+//咖啡
+class Coffee : public AbstractDrinking {
+public:
+    void Boil() {
+        cout << "Biol 农夫山泉" << endl;
+    }
+
+    void Brew() {
+        cout << "冲泡咖啡" << endl;
+    }
+
+    void PourInCup() {
+        cout << "倒入杯子" << endl;
+    }
+
+    void PourSomething() {
+        cout << "加入辅料" << endl;
+    }
+};
+
+//茶
+class Tea: public AbstractDrinking {
+public:
+    void Boil() {
+        cout << "Biol 矿泉水" << endl;
+    }
+
+    void Brew() {
+        cout << "冲泡茶" << endl;
+    }
+
+    void PourInCup() {
+        cout << "倒入杯子" << endl;
+    }
+
+    void PourSomething() {
+        cout << "加入辅料" << endl;
+    }
+};
+
+//工作
+void doWork(AbstractDrinking * abs) {
+    abs->makeDrink();
+    delete abs;
+}
+
+void test01() {
+    //制作咖啡
+    doWork(new Coffee);
+    //制作茶
+    doWork(new Tea);
+}
+
+int main() {
+    test01();
+    return 0;
+}
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Administrator/CLionProjects/Demo
+# Install script for directory: C:/Users/Administrator/CLionProjects/CLearn
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/Administrator/CLionProjects/Demo/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/Administrator/CLionProjects/CLearn/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
